fc05c4c3285458e8192e22443c992bfa3da70a2f
Stylix <anhduongfa@gmail.com>
2025-04-09 10:40:37 +0700

    feat: Init project

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+
+const items = ref<NavigationMenuItem[]>([
+    {
+        label: "Guide",
+        icon: "i-lucide-book-open",
+        to: '/',
+        children: [
+            {
+                label: 'Introduction',
+                description: 'Fully styled and customizable components for Nuxt.',
+                icon: 'i-lucide-house'
+            },
+            {
+                label: 'Installation',
+                description: 'Learn how to install and configure Nuxt UI in your application.',
+                icon: 'i-lucide-cloud-download'
+            },
+        ]
+    }
+])
+</script>
+
+<template>
+    <div>
+        <UNavigationMenu :items="items" class="w-full justify-center" />
+        <div>
+            <slot />
+        </div>
+    </div>
+
+
+</template>
