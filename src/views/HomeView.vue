8c1dc29577fa3a9d51b9e8707979b91a79051513
Stylix <anhduongfa@gmail.com>
2025-04-23 10:51:24 +0700

    init: Reinit project using vuejs

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="bg-red-600">Hello World</h1>
+  </main>
+</template>
