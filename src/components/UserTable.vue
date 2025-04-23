b7590507cc22c21c58ea3d22e5a69aab2f6a74d7
Stylix <anhduongfa@gmail.com>
2025-04-23 17:39:13 +0700

    feat: implement login page and testing user management table

---
+++ src/components/UserTable.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+</script>
+
+<template>
+  <Table>
+    <TableCaption>A table for management user</TableCaption>
+  </Table>
+</template>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <h1 class="bg-red-600">Hello World</h1>
-  </main>
-</template>
